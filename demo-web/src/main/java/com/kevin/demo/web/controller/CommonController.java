a7fd29a142157922f604acd6acac6292a6d7ff8a
liuwentao <liuwentao@oneplus.cn>
2017-02-28 09:44:47 +0800

    集成权限控制和验证码

--- demo-web/src/main/java/com/kevin/demo/web/controller/CommonController.java
+++ demo-web/src/main/java/com/kevin/demo/web/controller/CommonController.java
@@ -1,7 +1,3 @@
-/******************************************************************************
- * Copyright (C) 2013 - 2017 ShenZhen oneplus Technology Co.,Ltd All Rights Reserved.
- * 本软件为深圳万普拉斯科技有限公司开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件 .
- *****************************************************************************/
 package com.kevin.demo.web.controller;
 
 import com.google.code.kaptcha.Constants;
