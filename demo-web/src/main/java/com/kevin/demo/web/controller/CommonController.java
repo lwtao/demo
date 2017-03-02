e868cc7ac96bc6f5373859747e1a55ba8a84cf06
liuwentao <liuwentao@oneplus.cn>
2017-03-02 08:57:18 +0800

    增加图片上传功能

--- demo-web/src/main/java/com/kevin/demo/web/controller/CommonController.java
+++ demo-web/src/main/java/com/kevin/demo/web/controller/CommonController.java
@@ -2,8 +2,11 @@ package com.kevin.demo.web.controller;
 
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -12,6 +15,8 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author liuwentao
@@ -24,6 +29,27 @@ public class CommonController {
 
     @Resource
     private Producer captchaProducer;
+
+    @RequestMapping("upload")
+    public String uploadFile(@RequestParam("uploadfile") CommonsMultipartFile file, HttpServletRequest request) {
+        // MultipartFile是对当前上传的文件的封装，当要同时上传多个文件时，可以给定多个MultipartFile参数(数组)
+        if (!file.isEmpty()) {
+            String type = file.getOriginalFilename().substring(file.getOriginalFilename().indexOf("."));// 取文件格式后缀名
+            String filename = System.currentTimeMillis() + type;// 取当前时间戳作为文件名
+            String path = request.getSession().getServletContext().getRealPath("/upload/" + filename);// 存放位置
+            File destFile = new File(path);
+            try {
+                // FileUtils.copyInputStreamToFile()这个方法里对IO进行了自动操作，不需要额外的再去关闭IO流
+                FileUtils.copyInputStreamToFile(file.getInputStream(), destFile);// 复制临时文件到指定目录下
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "ok";
+        } else {
+            return "error";
+        }
+    }
 
     @RequestMapping("/captcha.jpg")
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
